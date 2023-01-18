21a29c49ef0cd4a8e87220be2ca09a14f89d4b7d
Beniamin TOLAN <39988307+xTbeniaminTx@users.noreply.github.com>
2023-01-18 15:06:01 +0100

    comment added

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -12,7 +12,11 @@ public class Greeter {
 
   }
 
-  //TODO: Add javadoc comment
+  /**
+   *
+   * @param someone
+   * @return
+   */
   public String greet(String someone) {
     return String.format("Hello, %s!", someone);
   }
